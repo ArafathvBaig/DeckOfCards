84fc206d8df1aa615dbebccaf63b4dc746fec796
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-13 19:32:06 +0530

    Added: Java File to Branch 1

---
+++ src/com/WorkShopDeckOfCards/DeckOfCards.java
@@ -0,0 +1,24 @@
+package com.WorkShopDeckOfCards;
+
+public class DeckOfCards 
+{
+	public static void main(String[] args) 
+	{
+		String deck[] = new String[52];
+		int k=0;
+		String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",  "Ace" };
+		for(int i=0; i<suits.length; i++)
+		{
+			for(int j=0; j<ranks.length; j++)
+			{
+				deck[k]=suits[i].concat(ranks[j]);
+				k++;
+			}
+		}
+		for (int i=0; i<deck.length; i++)
+		{
+			System.out.println(deck[i]);
+		}
+	}
+}
