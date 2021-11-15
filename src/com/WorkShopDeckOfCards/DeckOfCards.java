66fb84cfe14a16eb8cab557835a22bfd74d8479a
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-11-15 11:26:23 +0530

    Updated: Files With UC5

--- src/com/WorkShopDeckOfCards/DeckOfCards.java
+++ src/com/WorkShopDeckOfCards/DeckOfCards.java
@@ -6,7 +6,13 @@ public class DeckOfCards
 {
 	Scanner sc = new Scanner(System.in);
 	String deck[] = new String[52];
+	String player1[] = new String[9];
+	String player2[] = new String[9];
+	String player3[] = new String[9];
+	String player4[] = new String[9];	
 	int k=0;
+	int j=0;
+	int i=0;
 	int index;
 	String temp;
 	public void deck()
@@ -27,15 +33,7 @@ public class DeckOfCards
 		}
 		shuffle(deck);
 	}
-	public void sequence(int n)
-	{
-		for(int i=1; i<n; i++)
-		{
-			System.out.println("Choose "+i+" player to play");
-			int choice = sc.nextInt();
-			System.out.println(n+" Player Plays at Postition "+i);
-		}		
-	} 
+	
 	public void shuffle(String deck1[])
 	{
 		for (int i = 0; i < deck1.length; i++) 
@@ -45,12 +43,67 @@ public class DeckOfCards
 			deck1[i] = deck1[index];
 			deck1[index] = temp;
 		}
+		System.out.println(" ");
 		System.out.println("Shuffled Deck Is: ");
 		for (int i=0; i<deck1.length; i++)
 		{
 			System.out.println(deck1[i]);
+		}	
+	}
+	
+	public void sequence(int n)
+	{
+		int choice; 
+		for(int i=0; i<n; i++)
+		{
+			System.out.println("Choose "+(i+1)+" player to play");
+			choice = sc.nextInt();
+			System.out.println(choice+" Player Plays at Postition "+(i+1));
+			distribute(choice);
+		}
+	} 
+	
+	public void distribute(int choice)
+	{
+		
+		if(choice==1)
+		{
+			for(int j=0; j<9; j++)
+			{
+				player1[j] = deck[i];
+				System.out.println(player1[j]);
+				i++;
+			}				
+		}
+		if(choice==2)
+		{
+			for(int j=0; j<9; j++)
+			{
+				player2[j] = deck[i];
+				System.out.println(player2[j]);
+				i++;
+			}
+		}
+		if(choice==3)
+		{
+			for(int j=0; j<9; j++)
+			{
+				player3[j] = deck[i];
+				System.out.println(player3[j]);
+				i++;
+			}
+		}
+		if(choice==4)
+		{
+			for(int j=0; j<9; j++)
+			{
+				player4[j] = deck[i];
+				System.out.println(player4[j]);
+				i++;
+			}
 		}
 	}
+	
 	public static void main(String[] args) 
 	{
 		DeckOfCards doc = new DeckOfCards();
@@ -64,3 +117,17 @@ public class DeckOfCards
 		doc.sequence(n);	
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
